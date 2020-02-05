482c8156a548525d08618f72f5593b4f6b4a63d6
wangqiang <123456>
2020-02-05 11:54:53 +0800

    scanUI

--- myapplication6/src/main/java/com/example/myapplication6/MainScanActivity.java
+++ myapplication6/src/main/java/com/example/myapplication6/MainScanActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication6;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication6.ui.main.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,22 +12,60 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-public class MainScanActivity extends AppCompatActivity {
+public class MainScanActivity extends AppCompatActivity implements View.OnClickListener {
+    String[] scanTags = {"扫码", "扫证件", "翻译"};
+    String[] tipTags = {"通用", "地标"};
+    String [][]tags={scanTags,tipTags};
+    int position;
+    Button button1, button2;
+    TabLayout tabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_main);
 
-        TabLayout tabs = findViewById(R.id.tabs);
+        tabs = findViewById(R.id.tabs);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
         FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                CharSequence text = tabs.getTabAt(tabs.getSelectedTabPosition()).getText();
+                Snackbar.make(view, ""+text, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                if (position!=0){
+                    position=0;
+                    onBottomCheckChanged();
+                }
+                break;
+            case R.id.button2:
+                if (position!=1){
+                    position=1;
+                    onBottomCheckChanged();
+                }
+                break;
+
+        }
+    }
+   void onBottomCheckChanged(){
+       tabs.removeAllTabs();
+       String[] currentTags = tags[position];
+       for (int i = 0; i< currentTags.length; i++){
+           TabLayout.Tab tab = new TabLayout.Tab();
+           tab.setText(currentTags[i]);
+           tabs.addTab(tab);
+       }
+   }
 }
